3d64ab87b8d32b9188f3a4587d42b8e6ee857d32
Elena Shvetsova <Be-beheaded@yandex.ru>
2025-09-16 23:32:49 +0300

    homework_9

---
+++ homework_9-10/homework_10.py
@@ -0,0 +1,29 @@
+import random
+
+
+def main():
+    # создание массива из целых чисел
+    random_list = []
+    for i in range(100000):
+        random_list.append(random.randint(1, 1000000))
+
+    # создание массива из словарей
+    random_dict_list = []
+    for i in range(100000):
+        random_dict_list.append({"num_1": random.randint(1, 1000000), "num_2": random.randint(1, 1000000)})
+
+    # сортировка .sort() и лямбда-функции
+    random_dict_list.sort(key=lambda n: n["num_1"])
+    random_dict_list.sort(key=lambda n: n["num_2"])
+
+# создание алгоритма сортировки пузырьком
+def bubble_sort(random_list):
+    length = len(random_list) - 1
+    for i in range(length):
+        for j in range(length - i):
+            if random_list[j] > random_list[j + 1]:
+                random_list[j], random_list[j + 1] = random_list[j + 1], random_list[j]
+
+
+if __name__ == "__main__":
+    main()
